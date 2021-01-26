fa3a0f46967f7ffe89a6e49e9f5dd049c487b047
whitton <mark.whitton@gmail.com>
2021-01-26 12:15:07 -0500

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        public UsersController(DataContext context)
+        {
+            Context = context;
+        }
+
+        public DataContext Context { get; }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> Users()
+        {
+            var users = Context.Users.ToList();
+            return users;
+        }       
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> FetchUser(int id)
+        {
+            var user = Context.Users.Where(x => x.Id == id).FirstOrDefault();
+            return user;
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public AppUser()
+        {
+        }
+
+        /// <summary>
+        /// Unique id for the user 
+        /// Entity Framework will recognise this as an Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the user
+        /// </summary>
+        public string UserName { get; set; }
+
+
+       
+    }
+}
